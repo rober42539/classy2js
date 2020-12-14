3cbc4e7d48381ed6f882a7849a0cb325a88e0e49
Robert M Campbell <robert.rcampbell@gmail.com>
2020-12-14 13:22:13 +0700

    Set up the template

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Classy2 JS Logo" src="./assets/logo.png">
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +14,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #ffa1a8;
+  text-decoration: none; 
+}
+
+#nav a.router-link-exact-active {
+  color: #e47fa8;
 }
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>About Classy2 JS</h1>
+  </div>
+</template>
